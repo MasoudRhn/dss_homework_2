da01227cb598e3b7fa73828a9051b367810b2856
masoud.rahnama <masoud.rahnama@fau.de>
2024-11-10 15:06:57 +0100

    Added unit test

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,46 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Random_int, Random_operator, Operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_random_int(self):
+
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+        for _ in range(1000):
+            rand_num = Random_int(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_operator(self):
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        for _ in range(1000):
+            operator = Random_operator()
+            self.assertIn(
+                operator, ["+", "-", "*"], "Operator should be one of ['+', '-', '*']"
+            )
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+    def test_operation(self):
+
+        test_cases = [
+            (5, 2, "+", "5 + 2", 7),
+            (10, 3, "-", "10 - 3", 7),
+            (4, 3, "*", "4 * 3", 12),
+        ]
+
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = Operation(num1, num2, operator)
+            self.assertEqual(
+                problem,
+                expected_problem,
+                f"Expected problem '{expected_problem}' but got '{problem}'",
+            )
+            self.assertEqual(
+                answer,
+                expected_answer,
+                f"Expected answer '{expected_answer}' but got '{answer}'",
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
