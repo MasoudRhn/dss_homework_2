670ff210bcbfbd646b1bb293ed3e5621bbc992a3
masoud.rahnama <masoud.rahnama@fau.de>
2024-11-10 15:28:20 +0100

    Pip install Setup added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    test_suite="tests",
+    description="A simple math quiz game package",
+    author="Masoud_R",
+    author_email="",
+    url="https://github.com/MasoudRhn/dss_homework_2.git",  # Replace with your actual repository URL
+    classifiers=[],
+    python_requires=">=3.6",
+)
